108045c792dd79968c6986afa09d903898bee239
Gilgamesh Athoraya <GHTH@simcorp.com>
2020-05-08 17:26:55 +0200

    added test groups

---
+++ Test/TestGroup001.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Dyalog;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test {
+    [TestClass]
+    public class TestGroup001 {
+        [TestMethod]
+        public void TestMethod1() {
+            var interpreterSettings = new Dictionary<string, string> {
+                ["RUNASSERVICE"] = "2",
+                ["MAXWS"] = "1G"
+            };
+            for (int i = 0; i < 1000; i++) {
+                var interpreter = new DyalogInterpreter(null, interpreterSettings) {
+                    SingleThreaded = true,
+                    DeleteOnUnload = true
+                };
+
+                try {
+                    var apl = new AplGroup001.SubSpace.A1(interpreter);
+                    var result = apl.Echo(i);
+                    Assert.AreEqual(i, result);
+                } finally {
+                    interpreter.Unload();
+                }
+            }
+        }
+    }
+}
