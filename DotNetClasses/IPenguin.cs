9b6ce6fe77c48ed0be9dd26cae580c3cdec83544
Gilgamesh Athoraya <GHTH@simcorp.com>
2020-05-12 16:49:57 +0200

    Added interface tests

---
+++ DotNetClasses/IPenguin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetClasses {
+    public interface IPenguin {
+        IHerd Herd { get; }
+        string Fly { get; }
+    }
+}
--- Test/TestNameScopes.cs
+++ Test/TestNameScopes.cs
@@ -6,6 +6,16 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace Test {
     [TestClass]
     public class TestNameScopes {
+
+        private DyalogInterpreter MultiHostNullNull() {
+            //return new DyalogInterpreter(null, null) {
+            return new DyalogInterpreter(".\\dyalog180_64_unicode.dll", null) {
+                SingleThreaded = true,
+                DeleteOnUnload = true
+            };
+        }
+
+
         [TestMethod]
         public void EchoNoDispose() {
             var interpreterSettings = new Dictionary<string, string> {
@@ -52,38 +62,228 @@ namespace Test {
         }
 
         [TestMethod]
-        public void NestedInstanceMultiHostNullNull() {
-            var interpreter = new DyalogInterpreter(null, null) {
-                SingleThreaded = true,
-                DeleteOnUnload = true
-            };
+        public void NestedClassSameAssemblySimpleName() {
+            var int1 = MultiHostNullNull();
 
             try {
-                var apl = new AplGroup003.B2(1, interpreter);
-                Assert.AreEqual(1, apl.ID());
-                Assert.AreEqual(1, apl.B2ID());
-                apl.Init();
-                Assert.AreEqual(2, apl.B1ID());
-                Assert.AreEqual(2, apl.B2ID());
+                var apl1 = new AplGroup002.NestedClassSimpleName(1, int1);
+                Assert.AreEqual(1, apl1.ID());
             } finally {
-                interpreter.Unload();
-            }  
+                int1.Unload();
+            }
         }
 
         [TestMethod]
-        public void NestedInstanceMultiHostPathNull() {
+        public void NestedClassSameAssemblyQualifiedName() {
+            var int1 = MultiHostNullNull();
+
+            try {
+                var apl1 = new AplGroup002.NestedClassQualifiedName(1, int1);
+                Assert.AreEqual(1, apl1.ID());
+            } finally {
+                int1.Unload();
+            }
+        }
+
+        [TestMethod]
+        public void DerivedClassSameAssemblySimpleName() {
+            var int1 = MultiHostNullNull();
+
+            try {
+                var apl1 = new AplGroup002.DerivedClassSimpleName(1, int1);
+                Assert.AreEqual(1, apl1.ID());
+            } finally {
+                int1.Unload();
+            }
+        }
+
+        [TestMethod]
+        [Ignore]
+        public void DerivedClassSameAssemblyQualifiedName() {
+            var int1 = MultiHostNullNull();
+
+            try {
+                //var apl1 = new AplGroup002.DerivedClassQualifiedName(1, int1);
+                //Assert.AreEqual(1, apl1.ID());
+            } finally {
+                int1.Unload();
+            }
+        }
+
+        [TestMethod]
+        public void SimpleClassSingleInterpreter() {
+            var int1 = MultiHostNullNull();
+
+            try {
+                var apl1 = new AplGroup002.SimpleClass(1, int1);
+                Assert.AreEqual(1, apl1.ID());
+
+                var apl2 = new AplGroup002.SimpleClass(2, int1);
+                Assert.AreEqual(2, apl1.ID());
+                Assert.AreEqual(2, apl2.ID());
+            } finally {
+                int1.Unload();
+            }
+        }
+
+        [TestMethod]
+        public void SimpleClassSplitInterpreter() {
+            var int1 = MultiHostNullNull();
+            var int2 = MultiHostNullNull();
+
+            try {
+                var apl1 = new AplGroup002.SimpleClass(1, int1);
+                Assert.AreEqual(1, apl1.ID());
+
+                var apl2 = new AplGroup002.SimpleClass(2, int2);
+                Assert.AreEqual(1, apl1.ID());
+                Assert.AreEqual(2, apl2.ID());
+            } finally {
+                int1.Unload();
+                int2.Unload();
+            }
+        }
+
+        [TestMethod]
+        public void NestedInstanceSingleInterpreterSimpleName() {
+            var interpreter = MultiHostNullNull();
+
+            try {
+                var apl = new AplGroup003.NestedClassSimpleName(1, interpreter);
+                Assert.AreEqual(1, apl.ID());
+                Assert.AreEqual(1, apl.ThisID());
+                apl.Init();
+                Assert.AreEqual(2, apl.ID());
+                Assert.AreEqual(2, apl.NestedID());
+                Assert.AreEqual(2, apl.ThisID());
+            } finally {
+                interpreter.Unload();
+            }
+        }
+
+        [TestMethod]
+        public void NestedInstanceSingleInterpreterQualifiedName() {
+            var interpreter = MultiHostNullNull();
+
+            try {
+                var apl = new AplGroup003.NestedClassQualifiedName(1, interpreter);
+                Assert.AreEqual(1, apl.ID());
+                Assert.AreEqual(1, apl.ThisID());
+                apl.Init();
+                Assert.AreEqual(2, apl.ID());
+                Assert.AreEqual(2, apl.NestedID());
+                Assert.AreEqual(2, apl.ThisID());
+            } finally {
+                interpreter.Unload();
+            }
+        }
+
+        [TestMethod]
+        public void NestedInstanceSplitInterpreterSimpleName() {
+            var interpreter1 = MultiHostNullNull();
+            var interpreter2 = MultiHostNullNull();
+
+            try {
+                var apl1 = new AplGroup003.NestedClassSimpleName(1, interpreter1);
+                Assert.AreEqual(1, apl1.ID());
+                Assert.AreEqual(1, apl1.NestedID());
+                Assert.AreEqual(1, apl1.ThisID());
+                apl1.Init();
+                Assert.AreEqual(2, apl1.ID());
+                Assert.AreEqual(2, apl1.NestedID());
+                Assert.AreEqual(2, apl1.ThisID());
+
+                var apl2 = new AplGroup003.NestedClassSimpleName(11, interpreter2);
+                Assert.AreEqual(11, apl2.ID());
+                Assert.AreEqual(11, apl2.NestedID());
+                Assert.AreEqual(11, apl2.ThisID());
+                apl2.Init();
+                Assert.AreEqual(12, apl2.ID());
+                Assert.AreEqual(12, apl2.NestedID());
+                Assert.AreEqual(12, apl2.ThisID());
+            } finally {
+                interpreter1.Unload();
+                interpreter2.Unload();
+            }
+        }
+
+        [TestMethod]
+        public void NestedInstanceSplitInterpreterQualifiedName() {
+            var interpreter1 = MultiHostNullNull();
+            var interpreter2 = MultiHostNullNull();
+
+            try {
+                var apl1 = new AplGroup003.NestedClassQualifiedName(1, interpreter1);
+                Assert.AreEqual(1, apl1.ID());
+                Assert.AreEqual(1, apl1.ThisID());
+                apl1.Init();
+                Assert.AreEqual(2, apl1.ID());
+                Assert.AreEqual(2, apl1.NestedID());
+                Assert.AreEqual(2, apl1.ThisID());
+
+                var apl2 = new AplGroup003.NestedClassQualifiedName(11, interpreter2);
+                Assert.AreEqual(11, apl2.ID());
+                Assert.AreEqual(11, apl2.ThisID());
+                apl2.Init();
+                Assert.AreEqual(12, apl2.ID());
+                Assert.AreEqual(12, apl2.NestedID());
+                Assert.AreEqual(12, apl2.ThisID());
+            } finally {
+                interpreter1.Unload();
+                interpreter2.Unload();
+            }
+        }
+
+        [TestMethod]
+        public void NestedInstanceSingleHost() {
             var interpreter = new DyalogInterpreter(".\\dyalog180_64_unicode.dll", null) {
                 SingleThreaded = true,
                 DeleteOnUnload = true
             };
 
             try {
-                var apl = new AplGroup003.B2(1, interpreter);
+                var apl = new AplGroup003.NestedClassSimpleName(1, interpreter);
                 Assert.AreEqual(1, apl.ID());
-                Assert.AreEqual(1, apl.B2ID());
+                Assert.AreEqual(1, apl.NestedID());
+                Assert.AreEqual(1, apl.ThisID());
                 apl.Init();
-                Assert.AreEqual(2, apl.B1ID());
-                Assert.AreEqual(2, apl.B2ID());
+                Assert.AreEqual(2, apl.ID());
+                Assert.AreEqual(2, apl.NestedID());
+                Assert.AreEqual(2, apl.ThisID());
+            } finally {
+                interpreter.Unload();
+            }
+
+        }
+        
+        [TestMethod]
+        public void InterfaceTestWithCast() {
+            var interpreter = MultiHostNullNull();
+
+            try {
+                var apl = new AplInterfaceTest.Herd(interpreter);
+
+                Assert.AreEqual(0, apl.FlyWithCast().Length);
+                apl.AddPenguinWithCast();
+                Assert.AreEqual(1, apl.FlyWithCast().Length);
+
+            } finally {
+                interpreter.Unload();
+            }
+
+        }
+
+        [TestMethod]
+        public void InterfaceTestNoCast() {
+            var interpreter = MultiHostNullNull();
+
+            try {
+                var apl = new AplInterfaceTest.Herd(interpreter);
+
+                Assert.AreEqual(0, apl.FlyNoCast().Length);
+                apl.AddPenguinNoCast();
+                Assert.AreEqual(1, apl.FlyNoCast().Length);
+
             } finally {
                 interpreter.Unload();
             }
