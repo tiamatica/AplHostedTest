2ddbf8ec8a82536629b96e432b5d97f9a69892f5
Gilgamesh Athoraya <GHTH@simcorp.com>
2020-09-29 15:32:26 +0200

    add test for using shared code file

--- Test/CodeFileTests.cs
+++ Test/CodeFileTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using AplClasses;
 using Dyalog;
@@ -84,7 +85,7 @@ namespace Test {
             var interpreter2 = CreateInterpreter(new[] { codefile });
             var interpreter3 = CreateInterpreter(new[] { codefile });
             Assert.IsTrue(interpreter2.Unload());
-            Assert.IsTrue(interpreter3.Unload());
+            //Assert.IsTrue(interpreter3.Unload());
 
         }
 
@@ -95,8 +96,14 @@ namespace Test {
             if (Directory.Exists(folderpath)) Directory.Delete(folderpath, true);
             Directory.CreateDirectory(folderpath);
 
-            var fn1 = Path.Combine(folderpath, "fn1.aplf");
-            File.WriteAllLines(fn1, new string[] { "fn1←{1×⍵}" }, System.Text.Encoding.UTF8);
+            File.WriteAllLines(Path.Combine(folderpath, "fn1.aplf"), new string[] {
+                "fn1←{1×⍵}"
+            });
+            File.WriteAllLines(Path.Combine(folderpath, "fn2.aplf"), new string[] {
+                " fn2←{",
+                "    2×⍵",
+                " }"
+            });
 
             var interpreter = CreateInterpreter();
             var test1 = new CodeFile(interpreter);
@@ -111,6 +118,29 @@ namespace Test {
 
         }
 
-        
+        [TestMethod]
+        public void TestUseSharedCodeFileMultiHost() {
+            var codefile = Path.Combine(Path.GetTempPath(), "simplecodefile.dwx");
+            if (File.Exists(codefile)) File.Delete(codefile);
+            var interpreter = CreateInterpreter();
+            var aplc = new CodeFile(interpreter);
+            aplc.CreateSimpleCodeFile(codefile);
+            aplc.Dispose();
+            interpreter.Unload();
+
+            var interpreter1 = CreateInterpreter(new[] { codefile });
+            var interpreter2 = CreateInterpreter(new[] { codefile });
+            var interpreter3 = CreateInterpreter(new[] { codefile });
+
+            var aplc1 = new CodeFile(interpreter1);
+            var aplc2 = new CodeFile(interpreter2);
+            var aplc3 = new CodeFile(interpreter3);
+
+            aplc1.Execute("#.fn1 1");
+            aplc2.Execute("#.fn1 2");
+            aplc3.Execute("#.fn1 3");
+            
+        }
+
     }
 }
