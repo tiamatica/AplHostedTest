0b13da4a825956fa31894814bdbf4eedbb9ee335
Gilgamesh Athoraya <GHTH@simcorp.com>
2020-10-21 10:15:57 +0200

    add disposable tests

--- Test/DisposeTests.cs
+++ Test/DisposeTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AplClasses;
+using AplDisposable;
 using Dyalog;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -18,25 +19,21 @@ namespace Test {
         [Description("Check dispose method triggered simple class.")]
         public void DisposeSimpleClass() {
             var interpreter = GetInterpreter();
-            using (var test = new NewClass(42, interpreter)) {
-                try {
-                    var bigvar = test.Number;
-                } catch { }
+            using (var test = new AplDisposableAplBaseNonDisposable(interpreter)) {
             }
             interpreter.Unload();
         }
 
-        [TestMethod]
-        [Description("Check dispose method triggered on derived class.")]
-        public void DisposeDerivedClass() {
-            var interpreter = GetInterpreter();
-            using (var test = new ClassWithDispose(interpreter)) {
-                try {
-                    test.MakeBigVar(1000, 1000, "Tralala");
-                    var bigvar = test.GetBigVar();
-                } catch { }
-            }
-            interpreter.Unload();
-        }
+        //[TestMethod]
+        //[Description("Check dispose method triggered on derived class.")]
+        //public void DisposeDerivedClass() {
+        //    var interpreter = GetInterpreter();
+        //    using (var test = new (interpreter)) {
+        //        try {
+        //        } catch { }
+        //    }
+        //    interpreter.Unload();
+        //}
+
     }
 }
